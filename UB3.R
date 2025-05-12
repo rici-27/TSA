ec856eb7a2a1cb1c53f77e96a1ecfb0000d8c045
rici-27 <ricarda.buttmann@icloud.com>
2025-05-12 10:35:01 +0200

    ub3 angelegt

---
+++ UB3.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+
+data = read.table("citi.txt")
+View(data) # wird als Data Frame gespeichert
+
+# Plot
+ggplot(data, aes(x = time, y = trade)) +
+  geom_point()
+
+price_changes <- diff(data$trade)
+View(price_changes)
+
+nonzero_price_changes <- price_changes[price_changes != 0]
+View(nonzero_price_changes)
+
+acf(nonzero_price_changes)
